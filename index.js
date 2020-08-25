c43d5200ac5bad12d7618b1967588a84e535ce55
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-08-25 13:45:30 +0200

    Revert "Update index.js"
    This reverts commit ea05378378331714c3f26fe8854110e55cf9fc2f.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+scanButton.addEventListener("click", async () => {                                                        
 var tagID= ['27:73:65:a9','04:30:4f:b2:00:53:80','56:72:4d:a5'];
 var personID=['Nathan','Mickael','Giraffe'];
-scanButton.addEventListener("click", async () => {                                                        
 let tagValue=null;                                                                                      
 let badgeValue=null;                                                                                      
 let keyVal;                                                                                           
 var assoc=null;                                                                                   
 let funStarter=null;                                                                                          
-log("Welcome to a new version of this app");                                                            
+log("Ver 2.13");                                                            
 log("User clicked scan button");                                                                        
 try {                                                                                                   
   const reader1 = new NDEFReader();                                                                       
@@ -27,15 +27,16 @@ if(keyVal!=-1){
   person= personID[tagID.indexOf(tagValue)];
   match=true;
 }
-else {                                                                                                    
+else { 
+  beaconAssociation(tagValue,person);
   match=false;                                                                                          
 }
 if(match==true){                                                                                          
   log("We found a matching person for your tag");                                                         
   log(`> Name: ${person}`);
   if(confirm(`${person} Is that you?`)){                                                          
-    assoc = true;
-beaconVal=beaconAssociation();                                                                                  
+    assoc = true;  
+    window.alert("You can scan your beacon now");
 }
   else{
    log("This person was ignored . . .");
@@ -43,37 +44,36 @@ beaconVal=beaconAssociation();
 }
 }    
 });                                                                                                                                                                                           
-reader1.removeEventListener("reading",({message,serialNumber})=>{});
+//reader1.removeEventListener("reading",({message,serialNumber})=>{});
 if(funStarter==true){
-beaconVal=beaconAssociation();                                                                          
+beaconVal=beaconAssociation(tagValue,person);                                                                          
 }                                                                                                           
-try{                                                                                                                                                                                                            
+try{                                                                                                                                                                                                            }                                                                                                       catch{                                                                                                                                                                                                          }                                                                                                         }                                                                                                       catch (error) {                                                                                           log("Argh! " + error);                                                                                }                                                                                                     });                                                                                                   //   writeButton.addEventListener("click", async () => {
+//     log("User clicked write button");
+//     try {                                                                                            
+  //       const writer = new NDEFWriter();
+//       await writer.write("Hello world!");
+//       log("> Message written");
+//     } catch (error) {
+//       log("Argh! " + error);
+//     }
+//   });
 
-}                                                                                                       
-catch{                                                                                                                                                                                                          
-
-}                                                                                                         
-}                                                                                                       
-catch (error) {                                                                                           
-  log("Argh! " + error);                                                                                
-}                                                                                                     
-});                                                                                                  
-
-async function beaconAssociation() {
+async function beaconAssociation(beacon,person) {
 var beaconKey = ['04:82:3a:2a:ce:66:80','04:a9:34:2a:ce:66:80'];
 var beaconID = ['3EE66B','3EE694'];                                                                     
-let keyRead,row,beacon,addID;                                                                                                                                                    
+let keyRead,row,addID;                                                                                                                                                    
 log('Scanning for localisation beacon');
-if(window.alert("Scan beacon now")){
-  if(tagValue.onchange()){
+if(window.alert("Scan finished!")){
+await tagValue.onchange();
     return true;
-  }
+
 }                                                
-keyRead = String(serialNumber);
+keyRead = beacon;
 row = beaconKey.indexOf(keyRead);                                                                       
 if(row!=-1){
 beacon = beaconID[row];
-alert(`The beacon ${beacon} and the tag are correctly associated`)
+alert(`The beacon ${beacon} and the tag ${person} are correctly associated`)
 }                                                                                                       
 else{
 log('There is no matching beacon in the list');
@@ -86,11 +86,4 @@ return beaconID[row];
 }                                                                                                           
 }                                                                                                     
 }                                                                                                                                                                                                   
-} 
-addButton.addEventListener("click",async() =>{
-  let username;
-  log('> User clicked the "Add" button . . .');
-  if(confirm("Do you want to add a new user?")){
-username = prompt("Type in your username here: ");
-  }
-});                                                                                                                     
+}                                                                                                                      
