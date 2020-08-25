bc3139142b91e26865a239c1b754dc6248401b31
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-08-25 16:13:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ scanButton.addEventListener("click", async () => {
   let keyVal;                                                                                           
   var assoc=null;                                                                                   
   let funStarter=null;                                                                                          
-  log("Ver 2.13");                                                            
+  log("Ver 2.2");                                                            
   log("User clicked scan button");                                                                        
   try {                                                                                                   
     const reader1 = new NDEFReader();                                                                       
