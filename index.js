6f36161a76ef42c2ae23508e16612a85e0f940c0
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-08-25 10:08:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,102 +1,88 @@
-
-     scanButton.addEventListener("click", async () => {
-      let tagValue=null;
-      let badgeValue=null;
-      var assoc=null;
-        log("Another version");
-      log("User clicked scan button");
-      try {
-      const reader1 = new NDEFReader();
-        await reader1.scan();
-        log("> Scan started");
-        reader1.addEventListener("error", (event) => {
-          log(`Argh! ${event.message}`);
-        });
-        //while(tagValue==null){
-        reader1.addEventListener("reading", ({ message, serialNumber }) => {
-  log(`> Serial Number: ${serialNumber}`);
-  log(`> Records: (${message.records.length})`);
-      tagValue=String(serialNumber);
-      log(`> tagValue is: ${tagValue}`);
-    });
-  //}
-
-        log("Now place the badge you want to pair . . .");
-        try {
-      badgeValue = getBadgeValue();
-        }
-        catch {
-          log('There was an error when reading badge');
-        }
-      assoc = {tagValue:badgeValue};
-      log('You correctly associated the values ');
-      log(assoc);
-      
-
-      // assocData(tagValue);
-      // tagValue=null;
-
-          // if(tagValue=="04:82:3a:2a:ce:66:80"){
-          // log(`This is tag 3EE66B`);
-          // tagValue="3EE66B";
-          // assocData(tagValue);
-
-          // }
-          // if(tagValue=="04:a9:34:2a:ce:66:80"){
-          //               log('This is tag 3EE694');
-          //               tagValue="3EE694";
-          //               assocData(tagValue);
-          // }
-      }     
-      catch (error) {
-        log("Argh! " + error);
-      }
-    }); 
-  //   writeButton.addEventListener("click", async () => {
-  //     log("User clicked write button");     
-  //     try {
+scanButton.addEventListener("click", async () => {                                                        
+var tagID= ['27:73:65:a9','04:30:4f:b2:00:53:80','56:72:4d:a5'];
+var personID=['Nathan','Mickael','Giraffe'];
+let tagValue=null;                                                                                      
+let badgeValue=null;                                                                                      
+let keyVal;                                                                                           
+var assoc=null;                                                                                   
+let funStarter=null;                                                                                          
+log("Welcome to a new version of this app");                                                            
+log("User clicked scan button");                                                                        
+try {                                                                                                   
+  const reader1 = new NDEFReader();                                                                       
+  let match=false;                                                                                        
+  let person,beaconVal;                                                                                     
+  await reader1.scan();                                                                                   
+  log("> Scan started");                                                                                  
+  reader1.addEventListener("error", (event) => {
+    log(`Argh! ${event.message}`);
+  });                                                                                                    
+  funStarter= reader1.addEventListener("reading", ({ message, serialNumber }) => {
+log(`> Serial Number: ${serialNumber}`);                                                                
+log(`> Records: (${message.records.length})`);
+tagValue=String(serialNumber);
+log("> Looking for a matching pair in the database");
+keyVal = tagID.indexOf(tagValue);
+if(keyVal!=-1){
+  person= personID[tagID.indexOf(tagValue)];
+  match=true;
+}
+else {                                                                                                    
+  match=false;                                                                                          
+}
+if(match==true){                                                                                          
+  log("We found a matching person for your tag");                                                         
+  log(`> Name: ${person}`);
+  if(confirm(`${person} Is that you?`)){                                                          
+    assoc = true;
+beaconVal=beaconAssociation();                                                                                  
+}
+  else{
+   log("This person was ignored . . .");
+  match=false;                                                                                            
+}
+}    
+});                                                                                                                                                                                           
+reader1.removeEventListener("reading",({message,serialNumber})=>{});
+if(funStarter==true){
+beaconVal=beaconAssociation();                                                                          
+}                                                                                                           
+try{                                                                                                                                                                                                            }                                                                                                       catch{                                                                                                                                                                                                          }                                                                                                         }                                                                                                       catch (error) {                                                                                           log("Argh! " + error);                                                                                }                                                                                                     });                                                                                                   //   writeButton.addEventListener("click", async () => {
+//     log("User clicked write button");
+//     try {                                                                                            
   //       const writer = new NDEFWriter();
-  //       await writer.write("Hello world!");
-  //       log("> Message written");
-  //     } catch (error) {
-  //       log("Argh! " + error);
-  //     }
-  //   });   
-//   async function assocData(tagval){
-//   var reader2 = new NDEFReader;    
-//   reader2.scan();
-//   await reader2.addEventListener("reading",function thisfun() {
-//   log('Please scan your badge now . . .');
-//   let badge=null;
-//   badge=String(reader2.serialNumber);});
-//   log(`Your badge ID is: ${badge}`);
-//   if(badge!=null){
-//   if(confirm("Do you want to associate those values?")){
-//   assoc = {badge:tagval};
-//   log(`You succesfully associated tag ${tagval} with badge ${badge}`);
-//   }
-//   else{
-//   log('Start again if you want to pair a new badge');   
-//   }      
-// }    
-//   }     
-async function getBadgeValue(){
-  let badge = null;
-  try {
-  await findBadgeValue();
+//       await writer.write("Hello world!");
+//       log("> Message written");
+//     } catch (error) {
+//       log("Argh! " + error);
+//     }
+//   });
+
+async function beaconAssociation() {
+var beaconKey = ['04:82:3a:2a:ce:66:80','04:a9:34:2a:ce:66:80'];
+var beaconID = ['3EE66B','3EE694'];                                                                     
+let keyRead,row,beacon,addID;                                                                                                                                                    
+log('Scanning for localisation beacon');
+if(window.alert("Scan beacon now")){
+  if(tagValue.onchange()){
+    return true;
   }
-  catch {
-    log('Badge error');
-  }
-}
-async function findBadgeValue(){
-const reader2 = new NDEFReader;
-let badge=null;
-await reader2.scan;
-log('Badge scan has started . . .');
-while(badge==null){
-reader2.addEventListener("reading",({serialNumber}) => {
-badge = String(serialNumber);
-});}
-return badge;
-}
+}                                                
+keyRead = String(serialNumber);
+row = beaconKey.indexOf(keyRead);                                                                       
+if(row!=-1){
+beacon = beaconID[row];
+alert(`The beacon ${beacon} and the tag are correctly associated`)
+}                                                                                                       
+else{
+log('There is no matching beacon in the list');
+if(confirm('Would you like to add a new beacon?')){                                                       
+  beaconKey[beaconKey.length+1]=keyRead;                                                           
+   addID=prompt("Type in the ID of the beacon")
+if(addID!=null){
+beaconID[beaconID.length+1]=addID;                                                                      
+return beaconID[row];                                                                                 
+}                                                                                                           
+}                                                                                                     
+}                                                                                                                                                                                                   
+}                                                                                                                      
