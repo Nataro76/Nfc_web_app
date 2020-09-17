77ab190ed96a9ca3766f9b429fe75fde53ec64e7
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-09-17 14:54:16 +0200

    Update tagAssoc.js

--- tagAssoc.js
+++ tagAssoc.js
@@ -7,10 +7,12 @@ this.tagID= ['27:73:65:a9','04:30:4f:b2:00:53:80','56:72:4d:a5'];
 this.personID=['Nathan','Visiteur 096','Giraffe'];
 this.beaconKey = ['04:82:3a:2a:ce:66:80','04:a9:34:2a:ce:66:80','5f:46:8b:73:dc:5e:eb'];
 this.beaconID = ['3EE66B','3EE694','New high-tech beacon']; 
+this.tempStorage;
 } 
 
 tagAssoc.prototype.readMessage= function(msg){
-
+let message = String(msg);
+let ADDR = message.match(/(\d+)/);
 }
 
 tagAssoc.prototype.serialCheck=function(serial){
@@ -44,7 +46,7 @@ switch(match){
             }
             switch(check){
                 case (typeof check=='string'):
-                    this.unpair(this.tag,beacon);
+                    this.unpair(this.tag,check);
                     break;
                     case 'false':
                         return this.tag;
