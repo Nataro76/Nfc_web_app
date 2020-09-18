5c39c4868b5917387825e18702033f97a3473215
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-09-18 18:02:10 +0200

    Update tagAssoc.js

--- tagAssoc.js
+++ tagAssoc.js
@@ -7,7 +7,7 @@ this.tagID= ['27:73:65:a9','04:30:4f:b2:00:53:80','56:72:4d:a5'];
 this.personID=['Nathan','Visiteur 096','Giraffe'];
 this.beaconKey = ['04:82:3a:2a:ce:66:80','04:a9:34:2a:ce:66:80','5f:46:8b:73:dc:5e:eb'];
 this.beaconID = ['3EE66B','3EE694','New high-tech beacon']; 
-this.tempStorage={data:null,type:null};
+this.tempStorage={data:'null',type:'null'};
 } 
 
 tagAssoc.prototype.readMessage= function(msg){
@@ -17,24 +17,24 @@ return ADDR;
 }
 
 async function storToTemp(data,type){
-//     switch(this.tempStorage.type){
-//         case 'beacon':
-//         if(confirm(`Do you want to associate ${this.tempStorage.data} with ${data} ?`)){
-//         pairToBeacon(data,this.tempStorage.data);
-//         this.tempStorage=null;
-//         }
+    switch(this.tempStorage.type){
+        case 'beacon':
+        if(confirm(`Do you want to associate ${this.tempStorage.data} with ${data} ?`)){
+        pairToBeacon(data,this.tempStorage.data);
+        this.tempStorage=null;
+        }
 
-//     case'tag':
-//         if(confirm(`Do you want to associate ${this.tempStorage.data} with ${data} ?`)){
-//         pairToBeacon(this.tempStorage.data,data);
-//         this.tempStorage=null;
-//         }
-// default:
+    case'tag':
+        if(confirm(`Do you want to associate ${this.tempStorage.data} with ${data} ?`)){
+        pairToBeacon(this.tempStorage.data,data);
+        this.tempStorage=null;
+        }
+default:
     window.alert("I'm Batman");
-    this.tempStorage.data=data;
-    this.tempStorage.type=type;
+    this.tempStorage.data=String(data);
+    this.tempStorage.type=String(type);
 window.alert(this.tempStorage); 
-// }
+}
 }
 
 tagAssoc.prototype.serialCheck=function(serial){
