c5ac5c17b112f87ea59cafae24a5fb7fd5bf9cc8
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-09-18 15:54:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 scanButton.addEventListener("click", async () => {      
   //almost everything is displayed in the log() for testing purposes but it has no use and can be erased                                                                                                                                                                          
-  log("Ver 3.87/troubleshooting");                                                            
+  log("Ver 3.88/troubleshooting");                                                            
   log("User clicked scan button");    
 
   try {          
