79dc2d2104e1951090ea39d8da1b730585d03ecd
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-09-28 15:13:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-scanButton.addEventListener("click", async () => {      
-  //almost everything is displayed in the log() for testing purposes but it has no use and can be erased                                                                                                                                                                          
-  log("Ver 4.68");                                                            
-  log("User clicked scan button");    
+// scanButton.addEventListener("click", async () => {      
+//   //almost everything is displayed in the log() for testing purposes but it has no use and can be erased                                                                                                                                                                          
+//   log("Ver 4.61");                                                            
+//   log("User clicked scan button");    
 
-  try {          
-    let tagValue='no Tag';
-    let msgValue,msgType;
-    let tagObj=null; 
-    let beaconObj=null;                                                                                        
-    const reader1 = new NDEFReader();                                                                                                                                                           
-    await reader1.scan();                                                                                   
-    log("> Scan started");                                                                                  
-    reader1.addEventListener("error", (event) => {
-      log(`Argh! ${event.message}`);
-    });                
-    var myTag = new tagAssoc();
-reader1.addEventListener("reading", ({ message, serialNumber }) => {  
-  log(`> Serial Number: ${serialNumber}`);                                                                
-  log(`> Records: (${message.records.length})`);
-  for (const record of message.records) {
-    log(`> Record type:   ${record.recordType}`);
-     switch(record.recordType){
-        case "text":
-  console.assert(record.recordType === "text");
-  const textDecoder = new TextDecoder(record.encoding);
-  msgValue= `Text: ${textDecoder.decode(record.data)} (${record.lang})`;
-  break;
-       default:
-       msgValue=0;
-//this is all to compute the message
-}
+//   try {          
+//     let tagValue='no Tag';
+//     let msgValue,msgType;
+//     let tagObj=null; 
+//     let beaconObj=null;                                                                                        
+//     const reader1 = new NDEFReader();                                                                                                                                                           
+//     await reader1.scan();                                                                                   
+//     log("> Scan started");                                                                                  
+//     reader1.addEventListener("error", (event) => {
+//       log(`Argh! ${event.message}`);
+//     });                
+//     var myTag = new tagAssoc();
+// reader1.addEventListener("reading", ({ message, serialNumber }) => {  
+//   log(`> Serial Number: ${serialNumber}`);                                                                
+//   log(`> Records: (${message.records.length})`);
+//   for (const record of message.records) {
+//     log(`> Record type:   ${record.recordType}`);
+//      switch(record.recordType){
+//         case "text":
+//   console.assert(record.recordType === "text");
+//   const textDecoder = new TextDecoder(record.encoding);
+//   msgValue= `Text: ${textDecoder.decode(record.data)} (${record.lang})`;
+//   break;
+//        default:
+//        msgValue=0;
+// //this is all to compute the message
+// }
      
-     }
-  if(msgValue==0){
-    tagValue=String(serialNumber);
-    tagObj=myTag.serialCheck(tagValue);
-  }
-  else if(typeof msgValue== 'string'){
-    beaconObj =myTag.readMessage(msgValue);
-  }
-});
-  }                                                                                                                                                                                                                                                                                     
+//      }
+//   if(msgValue==0){
+//     tagValue=String(serialNumber);
+//     tagObj=myTag.serialCheck(tagValue);
+//   }
+//   else if(typeof msgValue== 'string'){
+//     beaconObj =myTag.readMessage(msgValue);
+//   }
+// });
+//   }                                                                                                                                                                                                                                                                                     
   
-  catch (error) {                                                                                           
-    log("Argh! " + error);                                                                                
-  }                                                                                                     
-});                                                                                                   
+//   catch (error) {                                                                                           
+//     log("Argh! " + error);                                                                                
+//   }                                                                                                     
+// });                                                                                                   
 
 
 unpairButton.addEventListener("click",async() =>{
@@ -67,7 +67,8 @@ if(confirm(`Have you scanned ${tagValue} ?`)){
 
 addButton.addEventListener("click",async() =>{
   clear();
+  let check = new tagAssoc();
 let pers= window.prompt('Please enter a new name to associate');
-serialCheck(pers);
+check.serialCheck(pers);
 
 })
