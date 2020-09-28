fc76453dd624ba2f65e4fc348c93b7ac9f4ae9b0
Nataro76 <69911992+Nataro76@users.noreply.github.com>
2020-09-28 15:32:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-// scanButton.addEventListener("click", async () => {      
-//   //almost everything is displayed in the log() for testing purposes but it has no use and can be erased                                                                                                                                                                          
-//   log("Ver 4.61");                                                            
-//   log("User clicked scan button");    
+scanButton.addEventListener("click", async () => {      
+  //almost everything is displayed in the log() for testing purposes but it has no use and can be erased                                                                                                                                                                          
+  log("Ver 4.68");                                                            
+  log("User clicked scan button");    
 
-//   try {          
-//     let tagValue='no Tag';
-//     let msgValue,msgType;
-//     let tagObj=null; 
-//     let beaconObj=null;                                                                                        
-//     const reader1 = new NDEFReader();                                                                                                                                                           
-//     await reader1.scan();                                                                                   
-//     log("> Scan started");                                                                                  
-//     reader1.addEventListener("error", (event) => {
-//       log(`Argh! ${event.message}`);
-//     });                
-//     var myTag = new tagAssoc();
-// reader1.addEventListener("reading", ({ message, serialNumber }) => {  
-//   log(`> Serial Number: ${serialNumber}`);                                                                
-//   log(`> Records: (${message.records.length})`);
-//   for (const record of message.records) {
-//     log(`> Record type:   ${record.recordType}`);
-//      switch(record.recordType){
-//         case "text":
-//   console.assert(record.recordType === "text");
-//   const textDecoder = new TextDecoder(record.encoding);
-//   msgValue= `Text: ${textDecoder.decode(record.data)} (${record.lang})`;
-//   break;
-//        default:
-//        msgValue=0;
-// //this is all to compute the message
-// }
+  try {          
+    let tagValue='no Tag';
+    let msgValue,msgType;
+    let tagObj=null; 
+    let beaconObj=null;                                                                                        
+    const reader1 = new NDEFReader();                                                                                                                                                           
+    await reader1.scan();                                                                                   
+    log("> Scan started");                                                                                  
+    reader1.addEventListener("error", (event) => {
+      log(`Argh! ${event.message}`);
+    });                
+    var myTag = new tagAssoc();
+reader1.addEventListener("reading", ({ message, serialNumber }) => {  
+  log(`> Serial Number: ${serialNumber}`);                                                                
+  log(`> Records: (${message.records.length})`);
+  for (const record of message.records) {
+    log(`> Record type:   ${record.recordType}`);
+     switch(record.recordType){
+        case "text":
+  console.assert(record.recordType === "text");
+  const textDecoder = new TextDecoder(record.encoding);
+  msgValue= `Text: ${textDecoder.decode(record.data)} (${record.lang})`;
+  break;
+       default:
+       msgValue=0;
+//this is all to compute the message
+}
      
-//      }
-//   if(msgValue==0){
-//     tagValue=String(serialNumber);
-//     tagObj=myTag.serialCheck(tagValue);
-//   }
-//   else if(typeof msgValue== 'string'){
-//     beaconObj =myTag.readMessage(msgValue);
-//   }
-// });
-//   }                                                                                                                                                                                                                                                                                     
+     }
+  if(msgValue==0){
+    tagValue=String(serialNumber);
+    tagObj=myTag.serialCheck(tagValue);
+  }
+  else if(typeof msgValue== 'string'){
+    beaconObj =myTag.readMessage(msgValue);
+  }
+});
+  }                                                                                                                                                                                                                                                                                     
   
-//   catch (error) {                                                                                           
-//     log("Argh! " + error);                                                                                
-//   }                                                                                                     
-// });                                                                                                   
+  catch (error) {                                                                                           
+    log("Argh! " + error);                                                                                
+  }                                                                                                     
+});                                                                                                   
 
 
 unpairButton.addEventListener("click",async() =>{
   //This was just used to erase the association object, right now it does nothing
 clear();
-          log('>Version 2.1');
+          log('>Version 2');
 log('> User clicked the "unpair" button');
-// reader1.removeEventListener("reading", ({ message, serialNumber }));
+reader1.removeEventListener("reading", ({ message, serialNumber }));
 var readerUnpair = new NDEFReader();
 var tagUnpair = new tagAssoc();
 await readerUnpair.scan();
